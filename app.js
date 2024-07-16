86f901aa402e83583ee59eb20385c8482ed17776
Eduardo Garcia <84125798+edugn2298@users.noreply.github.com>
2024-07-16 14:46:25 -0400

    act

---
+++ app.js
@@ -0,0 +1,11 @@
+/*Realizar una pagina web que consuma una API (Cualquiera)
+
+La pagina debe tener:
+
+-vista de todos los datos
+-vista de un solo dato
+-debe tener una barra de busqueda
+-Debe verse bien
+-Debe ser responsive
+-Debe tener un manejo correcto de los errores
+-tener una modal que pida al usuario su nombre su apellido y su nombre de usuario y lo guarde en localStorage para ser mostrado en la vista de todos los datos para simular un login (opcional)*/
